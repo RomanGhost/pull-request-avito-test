bab3cab7ec2d692582549ae0d1b22c9a97e75b7a
gh0st <rildiarov@gmail.com>
2025-11-13 21:53:11 +0400

    add models

---
+++ internal/handler/model/error.go
@@ -0,0 +1,10 @@
+package model
+
+type ErrorResponse struct {
+	Error ErrorDetail `json:"error"`
+}
+
+type ErrorDetail struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
---
+++ internal/handler/model/pull_request.go
@@ -0,0 +1,20 @@
+package model
+
+import "time"
+
+type PullRequest struct {
+	PullRequestID     string     `json:"pull_request_id"`
+	PullRequestName   string     `json:"pull_request_name"`
+	AuthorID          string     `json:"author_id"`
+	Status            string     `json:"status"` // OPEN | MERGED
+	AssignedReviewers []string   `json:"assigned_reviewers"`
+	CreatedAt         *time.Time `json:"createdAt,omitempty"`
+	MergedAt          *time.Time `json:"mergedAt,omitempty"`
+}
+
+type PullRequestShort struct {
+	PullRequestID   string `json:"pull_request_id"`
+	PullRequestName string `json:"pull_request_name"`
+	AuthorID        string `json:"author_id"`
+	Status          string `json:"status"`
+}
